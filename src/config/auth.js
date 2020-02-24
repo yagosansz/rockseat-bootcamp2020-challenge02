8a279605d1064a88914cba2deb125f7c599c5e79
Yago Santos de Sousa <yagosansz@gmail.com>
2020-02-24 14:35:31 -0500

    Added sessions authentication

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '17b191dfa134859327fb98f7ba95e80d',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
 routes.get('/users', UserController.show);
+
+routes.post('/sessions', SessionController.store);
 
 export default routes;
