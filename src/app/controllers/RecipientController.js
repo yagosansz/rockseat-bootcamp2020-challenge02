2349193be530dbff4b7361207f8d455c9ac1be3e
Yago Santos de Sousa <yagosansz@gmail.com>
2020-02-25 10:26:19 -0500

    Added schema validation

--- src/app/controllers/RecipientController.js
+++ src/app/controllers/RecipientController.js
@@ -1,7 +1,26 @@
+import * as Yup from 'yup';
+
 import Recipient from '../models/Recipient';
 
 class RecipientController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      street_name: Yup.string().required(),
+      street_number: Yup.string().required(),
+      complement: Yup.string(),
+      state: Yup.string().required(),
+      city: Yup.string().required(),
+      postal_code: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed' });
+    }
+
     const recipientExists = await Recipient.findOne({
       where: { email: req.body.email },
     });
@@ -16,6 +35,21 @@ class RecipientController {
   }
 
   async edit(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      street_name: Yup.string(),
+      street_number: Yup.string(),
+      complement: Yup.string(),
+      state: Yup.string(),
+      city: Yup.string(),
+      postal_code: Yup.string(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed' });
+    }
+
     const { email } = req.body;
 
     let recipient = await Recipient.findByPk(req.recipientId);
--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,19 @@
+import * as Yup from 'yup';
+
 import User from '../models/User';
 
 class UserController {
   async show(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .email()
+        .required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed' });
+    }
+
     const userExists = await User.findOne({ where: { email: req.body.email } });
 
     if (!userExists) {
